a0c10a472c04953814a8c05107b1f540cf4447d9
Stanislav2022 <tsv5@ukr.net>
2022-07-14 01:07:58 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit (event) {
+    event.preventDefault()
+    const formEl = event.currentTarget.elements;
+    const email = formEl.email.value;
+    const password = formEl.password.value;
+    const formData = {email, password}
+    console.log(formData)
+   
+
+
+
+    if (email.value === "" || password.value === "") {
+        return alert("Все поля должны быть заполнены!");
+      }
+    
+    event.currentTarget.reset();
+}
