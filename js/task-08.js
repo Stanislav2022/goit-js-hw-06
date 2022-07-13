fadae73feb2e31d9c646f59df0eb53d36953a595
Stanislav2022 <tsv5@ukr.net>
2022-07-14 01:10:18 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -7,15 +7,13 @@ function onFormSubmit (event) {
     const formEl = event.currentTarget.elements;
     const email = formEl.email.value;
     const password = formEl.password.value;
-    const formData = {email, password}
-    console.log(formData)
-   
 
 
-
-    if (email.value === "" || password.value === "") {
+    if (email === "" || password === "") {
         return alert("Все поля должны быть заполнены!");
       }
+      const formData = {email, password}
+      console.log(formData)
     
     event.currentTarget.reset();
 }
