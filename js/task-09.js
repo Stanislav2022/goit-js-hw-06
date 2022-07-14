723739d5eb58ae4431091901412dcaf3ec37b139
Stanislav2022 <tsv5@ukr.net>
2022-07-14 15:51:08 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyEl = document.querySelector('body');
+const colorValueEl = document.querySelector('.color');
+const btnChangeColorEl = document.querySelector('.change-color');
+
+
+
+btnChangeColorEl.addEventListener('click', changeColorFunction);
+function changeColorFunction() {
+  getRandomHexColor();
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  colorValueEl.textContent = `${getRandomHexColor()}`;
+}
