87bcb89d22ebf97ca74e7f3ad3a92b1c683bdd3a
Stanislav2022 <tsv5@ukr.net>
2022-07-14 21:35:44 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,47 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// createBoxes(amount) {
+// }
+
+const inputEl = document.querySelector('input');
+const boxesEl = document.querySelector('#boxes')
+const buttonCreateEl = document.querySelector ('button[data-create]')
+
+
+inputEl.addEventListener('change', onInputChange);
+
+buttonCreateEl.addEventListener('click', onButtonCreateChange);
+
+let x = 0;
+let number = 0;
+
+function onInputChange (event) {
+  number = `${event.currentTarget.value}`
+  console.log(number)
+  }
+  
+function onButtonCreateChange (){
+ 
+  for (let i = 0; i < number ; i += 1) {
+
+    let divBox = document.createElement('div');
+    x += 10;
+    divBox.style.width = `${x}px`;
+    divBox.style.height = `${x}px`;
+    divBox.style.backgroundColor = `${getRandomHexColor()}`;
+    boxesEl.append(divBox)
+
+  }
+}
+
+
+
+
+
+ 
+
+
+
+// console.log(divBox)
